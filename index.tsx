03872e172a0f509be8f7dac0bc2083b4fe9064d7
jonathanRinciari <jrinciari@greencheckverified.com>
2019-09-29 22:42:36 -0400

    Cleaned up unused imports

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React, { Component, createRef } from 'react';
 import { render } from 'react-dom';
 import ColorPad from './components/ColorPad';
-import ColorPadAlt from './components/ColorPadAlt';
 import { SketchPicker } from 'react-color';
 
 import './style.css';
@@ -13,19 +12,10 @@ interface AppState {
 
 class App extends Component<AppProps, AppState> {
   ref: React.RefObject<HTMLDivElement> = createRef();
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: 'rgba(1,23,23,1)'
-    }
-  }
 
   update = ({hex, rgb: {r, g, b, a}}) => {
     const color = `rgba(${r}, ${g}, ${b}, ${a})`;
     this.ref.current.style.backgroundColor = color;
-    this.setState({
-      color
-    })
   }
   render() {
     return (
@@ -34,9 +24,8 @@ class App extends Component<AppProps, AppState> {
         <p>The color pad is updated in real time by accessing the DOM node directly</p>
         <div style={{display: 'flex'}}>
           <ColorPad ref={this.ref}/>
-          <ColorPadAlt color={this.state.color} />
         </div>
-        <SketchPicker onChange={e => this.update(e)} />
+        <SketchPicker onChange={(e: any) => this.update(e)} />
       </div>
     );
   }
